3063ead79edd4cf49525cd5f3f3b590b67587ade
cmtlyt <95839504+cmtlyt@users.noreply.github.com>
2024-06-01 13:51:10 +0800

    fix: change base (#5)

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,6 +3,6 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [{ path: '/', component: () => import('@/views/home.vue') }]
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('/teach-zhihu-live/'),
   routes,
 })
