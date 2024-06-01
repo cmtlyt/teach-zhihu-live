a8b4d098bc97b48b8f24ea7454233078e40b84f1
cmtlyt <cmtlyt@users.noreply.github.com>
2024-06-01 05:36:08 +0000

    deploy: 356e8b8556430b38b9bede6ae63296e5f35ce084

---
+++ assets/zh-KM20_PeF.js
@@ -0,0 +1,1 @@
+const o={home:{title:e=>{const{normalize:t}=e;return t(["test title"])}}};export{o as default};
---
+++ assets/zh-legacy-Pae2i_2A.js
@@ -0,0 +1,1 @@
+System.register([],(function(t,e){"use strict";return{execute:function(){t("default",{home:{title:function(t){return(0,t.normalize)(["test title"])}}})}}}));
