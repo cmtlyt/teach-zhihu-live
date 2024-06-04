ad68d1e3813ca8d3095f57040545598dcb8ef06c
cmtlyt <95839504+cmtlyt@users.noreply.github.com>
2024-06-04 12:17:40 +0800

    feat: 新增验证码api，新增返回值自动格式化配置 (#10)
    Co-authored-by: cmtlyt <cmt01964896@taobao.com>

--- public/mock/handler.js
+++ public/mock/handler.js
@@ -6,10 +6,7 @@ const mockHandler = {
         matcher: (item) => item.name === data.name,
       })
       if (user) {
-        return {
-          success: false,
-          message: '用户名已存在',
-        }
+        return { __format: true, status: 400, data: { message: '用户名已存在' } }
       }
       try {
         const result = await storage.insert('user', data)
@@ -18,22 +15,41 @@ const mockHandler = {
           message: result,
         }
       } catch (e) {
-        return {
-          success: false,
-          message: e.message,
-        }
+        return { __format: true, status: 500, data: { message: e.message } }
       }
     },
     async login({ data }) {
       const [user] = await storage.find('user', {
         matcher: (item) => item.name === data.name && item.password === data.password,
       })
-      if (!user)
-        return {
-          success: false,
-          message: '用户名或密码错误',
-        }
+      if (!user) return { __format: true, status: 400, data: { message: '用户名或密码错误' } }
       return { success: true, user }
+    },
+    async checkCaptcha({ data }) {
+      const { captcha, captchaId } = data
+      if (!captcha) return { __format: true, status: 400, data: { message: '验证码不能为空' } }
+      if (!captchaId) return { __format: true, status: 400, data: { message: '验证码ID不能为空' } }
+      const sessionInfo = await storage.findById('session', captchaId)
+      if (!sessionInfo) return { __format: true, status: 400, data: { message: '验证码不存在' } }
+      if (sessionInfo.expireTime < new Date()) {
+        await storage.remove('session', sessionInfo)
+        return { __format: true, status: 400, data: { message: '验证码已过期' } }
+      }
+      const sessionData = JSON.parse(sessionInfo.info)
+      if (sessionData.captcha !== captcha) return { __format: true, status: 400, data: { message: '验证码错误' } }
+      await storage.remove('session', sessionInfo)
+      return { success: true }
+    },
+  },
+  get: {
+    async captcha({ query }) {
+      if (!query.phone) return { __format: true, status: 400, data: { message: '手机号不能为空' } }
+      const captcha = randomString(6)
+      const sectionInfo = await storage.update('session', {
+        id: randomString(16),
+        info: JSON.stringify({ phone: query.phone, captcha }),
+      })
+      return { success: true, captchaId: sectionInfo.id, captcha }
     },
   },
 }
--- public/mock/utils.js
+++ public/mock/utils.js
@@ -2,3 +2,9 @@
 function getType(value) {
   return Object.prototype.toString.call(value).slice(8, -1).toLowerCase()
 }
+
+function randomString(len = 16, pre = '') {
+  const str = pre + Math.random().toString(36).slice(2)
+  if (str.length >= len) return str.slice(0, len)
+  return str + randomString(len - str.length, str)
+}
