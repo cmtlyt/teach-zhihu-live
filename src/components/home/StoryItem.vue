d9a071c35897378b024bffdd04fd64327947cca6
小胡 <68814876+is-xiaohu-sigh@users.noreply.github.com>
2024-07-27 20:33:20 +0800

    feat: 新增主页文章列表 (#19)
    * feat: 新增主页文章列表
    
    * feat: 修改类命名不规范
    
    * chore: 添加类块间空行
    
    * chore: 修改不规范
    
    * chore: 修改不规范
    
    * chore: 修改不规范

---
+++ src/components/home/StoryItem.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  content?: string
+  date?: Date
+  author?: string
+  tags?: string[]
+}>()
+// 倒计时函数
+// const startCountdown = () => {
+//   const interval = setInterval(() => {
+//     countdown.value--
+//     if (countdown.value <= 0) {
+//       clearInterval(interval)
+//     }
+//   }, 1000)
+// }
+// startCountdown()
+</script>
+
+<template>
+  <!-- <div class="item-box" v-if="countdown > 0" :active="countdown > 1">
+    <a-space direction="vertical" size="large" :style="{ width: '100%' }">
+      <a-skeleton :animation="animation">
+        <a-space direction="vertical" :style="{ width: '100%' }" size="large">
+          <a-skeleton-line :rows="3" />
+          <a-skeleton-shape size="small" />
+        </a-space>
+      </a-skeleton>
+    </a-space>
+  </div> -->
+  <div class="item-box">
+    <div class="item-title">
+      <a href="#" class="title">{{ title }}</a>
+    </div>
+    <div class="item-content">
+      <span>{{ content }}</span>
+      <a-space
+        ><a-link href="link">
+          阅读全文
+          <template #icon> <icon-down /></template>
+        </a-link>
+      </a-space>
+    </div>
+    <div class="item-actions">
+      <a-space>
+        <a-button type="primary">
+          <template #icon>
+            <icon-caret-up />
+          </template>
+          <!-- Use the default slot to avoid extra spaces -->
+          <template #default><span>赞同</span></template>
+        </a-button>
+        <a-button type="primary">
+          <template #icon>
+            <icon-caret-down />
+          </template>
+        </a-button>
+        <a-button type="text">
+          <template #icon>
+            <icon-message />
+          </template>
+          1,099条评论
+        </a-button>
+        <a-button type="text">
+          <template #icon>
+            <icon-send />
+          </template>
+          <span>分享</span>
+        </a-button>
+        <a-button type="text">
+          <template #icon>
+            <icon-star-fill />
+          </template>
+          <span>收藏</span>
+        </a-button>
+        <a-button type="text">
+          <template #icon>
+            <icon-heart-fill />
+          </template>
+          <span>喜欢</span>
+        </a-button>
+        <a-dropdown position="bottom" class="darpdown">
+          <a-button type="text">
+            <template #icon>
+              <icon-more />
+            </template>
+          </a-button>
+          <template #content>
+            <a-doption>举报</a-doption>
+            <a-doption>不感兴趣</a-doption>
+          </template>
+        </a-dropdown>
+      </a-space>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.arco-icon {
+  font-size: 16px;
+}
+
+.item-box {
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  border-bottom: 1px solid #ccc;
+
+  .item-title {
+    font-size: 1.6rem;
+    font-weight: 1000;
+
+    .title {
+      line-height: 1.6;
+    }
+  }
+
+  .item-content {
+    font-size: 1.5rem;
+    line-height: 1.67;
+  }
+
+  .item-actions {
+    height: 5.2rem;
+    margin-bottom: -1rem;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+  }
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,317 @@
 <script setup lang="ts">
+import { onDeactivated, onMounted, ref } from 'vue'
+
 import Navbar from '@/components/Navbar.vue'
+import StoryItem from '@/components/home/StoryItem.vue'
+const tab_item = ref(0)
+const items = ref([
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+  {
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  },
+])
+
+const isTabActive = (val: number) => {
+  return tab_item.value !== val ? 'tab-item' : 'tab-item tab-item-active'
+}
+
+const handleScrollLoad = () => {
+  const { clientHeight, scrollTop, scrollHeight } = document.documentElement
+  if (clientHeight + scrollTop > scrollHeight - 20) {
+    // items.value.push(...Array(10).fill({ message: 'Foo' }))
+    addNewStory()
+  }
+}
+
+const addNewStory = () => {
+  let newArray = new Array(10)
+  newArray.fill({
+    title: '为什么足浴按摩技师老是问客人是哪里人？',
+    content:
+      '小粉灯： 这不是第一个问题，第一个问题往往是： “你之前来过吗？” 因为她要通过你对这个问题的回答判断能不能这次就收你小费做不正规的内容； 许多店提供正规服…',
+    date: '',
+    author: '',
+    tags: '',
+  })
+  items.value.push(...newArray)
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScrollLoad)
+})
+
+onDeactivated(() => {
+  window.removeEventListener('scroll', handleScrollLoad)
+})
 </script>
 
 <template>
-  <Navbar />
+  <div class="box">
+    <Navbar class="nav-bar" />
+    <div class="main" ref="listRef">
+      <div class="left-main">
+        <div class="img"><img src="/public/banner.jpg" alt="" /></div>
+        <div class="list-box">
+          <ul class="tab">
+            <li :class="isTabActive(0)" @click="tab_item = 0">关注</li>
+            <li :class="isTabActive(1)" @click="tab_item = 1">推荐</li>
+            <li :class="isTabActive(2)" @click="tab_item = 2">热榜</li>
+            <li :class="isTabActive(3)" @click="tab_item = 3">视频</li>
+          </ul>
+          <StoryItem
+            v-for="(value, key) in items"
+            :title="value.title"
+            :content="value.content"
+            :date="value.date"
+            :author="value.author"
+            :tags="value.tags"
+            :key="key"
+          />
+        </div>
+      </div>
+      <div class="right-main"></div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.box {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  padding-top: 5.2rem;
+  background-color: #f4f6f9;
+  height: 100%;
+
+  .nav-bar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: white;
+    z-index: 1000;
+  }
+
+  .main {
+    display: flex;
+    width: 100vw;
+    justify-content: center;
+    margin-top: 1rem;
+
+    .left-main {
+      width: 67.8rem;
+      display: flex;
+      flex-direction: column;
+      margin-right: 1rem;
+      background-color: white;
+
+      .img {
+        box-shadow: -1px -1px #ccc 2px;
+      }
+
+      .list-box {
+        box-shadow: -1px -1px #ccc 2px;
+        width: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+
+        .tab {
+          width: 100%;
+          height: 52px;
+          display: flex;
+          justify-content: left;
+          align-items: center;
+          font-size: 16px;
+          color: #666;
+          box-sizing: border-box;
+          border-bottom: 1px solid #ccc;
+
+          .tab-item {
+            line-height: 52px;
+            width: 76px;
+            text-align: center;
+          }
+
+          .tab-item:hover {
+            color: rgb(77, 17, 160);
+            cursor: pointer;
+          }
+
+          .tab-item-active {
+            color: skyblue;
+          }
+        }
+      }
+    }
+
+    .right-main {
+      width: 29.8rem;
+    }
+  }
+}
+</style>
